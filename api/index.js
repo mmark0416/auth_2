42916687a3b23d6f724faea552b8e551828606c8
Majoros Jozsef Mark <mmark0416@gmail.com>
2023-12-25 20:55:38 +0100

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,10 +1,16 @@
 import express from "express";
 import "dotenv/config";
+import mongoose from "mongoose";
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+  try {
+    mongoose.connect(process.env.MONGO_URL);
+    console.log(`Server is listening on port ${port}`);
+  } catch (error) {
+    console.log(error);    
+  }
 });
